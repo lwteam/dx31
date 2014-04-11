a3c5acf730fdde79edd41ce28406a7ea3788d153
haierspi <haierspi@qq.com>
2014-04-12 01:51:02 +0800

    删除无用的部分

--- data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: b4c6e81fefe08846c4f02e33d62281e9
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 1310dcd82658268673a56b9eecfbde52
-
-$pluginsetting = array (
-);
-?>
