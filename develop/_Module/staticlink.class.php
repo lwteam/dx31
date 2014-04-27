b24c4029059ebd99c5dce7dfa37e72ffec289285
haierspi <haierspi@qq.com>
2014-04-28 06:57:37 +0800

    url test2

--- develop/_Module/staticlink.class.php
+++ develop/_Module/staticlink.class.php
@@ -131,6 +131,11 @@ class staticlink_viewthread_location {
 		 
 		$_G['setting']['seohead']= preg_replace('/forum\.php\?mod=viewthread(?!&fid)/','forum.php?mod=viewthread&fid='.$fid,$_G['setting']['seohead']);
 
+		echo'<pre>';
+		var_dump( $_G['gp_fid'] , $fid );
+		echo'</pre>';exit;
+			
+
 		if($_G['gp_fid'] != $fid){
 			loadcache('staticlink');
 			$staticlink = & $_G['cache']['staticlink'];
