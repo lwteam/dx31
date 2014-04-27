53c105f81cbd9e8d5b65bdd11ad3387e29bcdcd9
haierspi <haierspi@qq.com>
2014-04-27 20:46:54 +0800

    测试

--- source/function/function_core.php
+++ source/function/function_core.php
@@ -744,6 +744,7 @@ function loadcache($cachenames, $force = false) {
 	return true;
 }
 
+
 function dgmdate($timestamp, $format = 'dt', $timeoffset = '9999', $uformat = '') {
 	global $_G;
 	$format == 'u' && !$_G['setting']['dateconvert'] && $format = 'dt';
@@ -753,8 +754,6 @@ function dgmdate($timestamp, $format = 'dt', $timeoffset = '9999', $uformat = ''
 		$tformat = getglobal('setting/timeformat');
 		$dtformat = $dformat.' '.$tformat;
 		$offset = getglobal('member/timeoffset');
-		$sysoffset = getglobal('setting/timeoffset');
-		$offset = $offset == 9999 ? ($sysoffset ? $sysoffset : 0) : $offset;
 		$lang = lang('core', 'date');
 	}
 	$timeoffset = $timeoffset == 9999 ? $offset : $timeoffset;
@@ -762,28 +761,33 @@ function dgmdate($timestamp, $format = 'dt', $timeoffset = '9999', $uformat = ''
 	$format = empty($format) || $format == 'dt' ? $dtformat : ($format == 'd' ? $dformat : ($format == 't' ? $tformat : $format));
 	if($format == 'u') {
 		$todaytimestamp = TIMESTAMP - (TIMESTAMP + $timeoffset * 3600) % 86400 + $timeoffset * 3600;
-		//$s = gmdate(!$uformat ? $dtformat : $uformat, $timestamp);
-		$s = date('Y-m-d H:i:s', $timestamp);
+		$s = gmdate(!$uformat ? str_replace(":i", ":i:s", $dtformat) : $uformat, $timestamp);
 		$time = TIMESTAMP + $timeoffset * 3600 - $timestamp;
-
-
-
-		//一年前
-		$yeartimestamp = $todaytimestamp-86400*365;
-
 		if($timestamp >= $todaytimestamp) {
-			$return = '<span title="'.$s.'">'.date('H:i',$timestamp ).'</span>';
-		} elseif(($days = intval(($todaytimestamp - $timestamp) / 86400)) >= 0 && $days < 7) {
-			$return = ($days + 1).' '.$lang['day'].$lang['before'];
-			if(!defined('IN_MOBILE')) {
-				$return = '<span title="'.$s.'">'.$return.'</span>';
+			if($time > 3600) {
+				return '<span title="'.$s.'">'.intval($time / 3600).'&nbsp;'.$lang['hour'].$lang['before'].'</span>';
+			} elseif($time > 1800) {
+				return '<span title="'.$s.'">'.$lang['half'].$lang['hour'].$lang['before'].'</span>';
+			} elseif($time > 60) {
+				return '<span title="'.$s.'">'.intval($time / 60).'&nbsp;'.$lang['min'].$lang['before'].'</span>';
+			} elseif($time > 0) {
+				return '<span title="'.$s.'">'.$time.'&nbsp;'.$lang['sec'].$lang['before'].'</span>';
+			} elseif($time == 0) {
+				return '<span title="'.$s.'">'.$lang['now'].'</span>';
+			} else {
+				return $s;
 			}
-		} elseif ($timestamp > $yeartimestamp) {
-			$return = '<span title="'.$s.'">'.date('m-d',$timestamp ).'</span>';
+		} elseif(($days = intval(($todaytimestamp - $timestamp) / 86400)) >= 0 && $days < 7) {
+			if($days == 0) {
+				return '<span title="'.$s.'">'.$lang['yday'].'&nbsp;'.gmdate($tformat, $timestamp).'</span>';
+			} elseif($days == 1) {
+				return '<span title="'.$s.'">'.$lang['byday'].'&nbsp;'.gmdate($tformat, $timestamp).'</span>';
+			} else {
+				return '<span title="'.$s.'">'.($days + 1).'&nbsp;'.$lang['day'].$lang['before'].'</span>';
+			}
 		} else {
-			$return = '<span title="'.$s.'">'.date('Y',$timestamp ).lang('space', 'year').'</span>';
+			return $s;
 		}
-		return $return;
 	} else {
 		return gmdate($format, $timestamp);
 	}
